3918980eba37ce4c5ffeedd7fd838c19d3f1ffb5
tylerharter <tylerharter@gmail.com>
2019-01-09 11:15:27 -0600

    tyler dev env

---
+++ team/README.txt
@@ -0,0 +1,3 @@
+The directories here are for the dev environments of various OL team
+members.  Everybody can maintain their own directory, but they're
+still in this repo so people can borrow from each other.
---
+++ team/dev-tyler/tyler.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -x # echo commands
+set -e # fast fail
+export DEBIAN_FRONTEND=noninteractive
+
+apt -y install emacs-nox
+
+cd ~
+git clone https://github.com/tylerharter/tools.git
+
+cp ~/tools/.emacs ~/.emacs
+sed -i -e 's/git_co\///g' ~/.emacs
+
+cd ~
+git clone https://github.com/open-lambda/open-lambda.git
+cd open-lambda
+make
